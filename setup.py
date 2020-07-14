782dca086f791770d0da2391570c858774cd2431
rq0 <redisk0gg@gmail.com>
2020-07-14 08:38:04 +0700

    removed requirements to resolve libs in project

--- setup.py
+++ setup.py
@@ -33,9 +33,5 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
     install_requires=[
-        'channels==2.1.7',
-        'bleach==3.1.0',
-        'django>=2.0.9, <3',
-        'channels-redis==2.3.3',
     ]
 )
